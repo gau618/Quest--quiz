e85f4efe95255094223f2df25a1eea8c877dd15d
gaurav kannaujiya <22b2147@iitb.ac.in>
2025-07-03 06:36:19 +0000

    Disable ESLint during build to fix Render deploy

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,11 @@
-// .eslintrc.js
 module.exports = {
   root: true,
   extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
   rules: {
-    "@typescript-eslint/no-explicit-any": "off",     // Allow `any`
-    "@typescript-eslint/no-unused-vars": "warn",     // Warn, don’t fail
-    "@next/next/no-img-element": "off",              // Allow <img> tags
-    "react-hooks/exhaustive-deps": "warn",           // Warn instead of error
-    "prefer-const": "warn"                           // Optional: Warn unused lets
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@next/next/no-img-element": "off",
+    "react-hooks/exhaustive-deps": "warn",
+    "prefer-const": "warn"
   }
 };
